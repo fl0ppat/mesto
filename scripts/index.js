eef33578855ddee30870fb0598e08037970028b2
Floppy Di <klaven011@gmail.com>
2020-12-25 19:35:28 +0300

    feat: Upgrade UX popup closing

--- scripts/index.js
+++ scripts/index.js
@@ -30,110 +30,124 @@ const grid = document.querySelector('.grid-cards');
 const mestoTemplateContent = document.querySelector('#mesto').content;
 
 const init = () => {
-  editButton.addEventListener('click', () => createPopup('edit'));
-  addButton.addEventListener('click', () => createPopup('add'));
-  formEditUserProfile.addEventListener('submit', submitEditForm);
-  formAddCard.addEventListener('submit', submitAddForm);
+    editButton.addEventListener('click', () => createPopup('edit'));
+    addButton.addEventListener('click', () => createPopup('add'));
+    formEditUserProfile.addEventListener('submit', submitEditForm);
+    formAddCard.addEventListener('submit', submitAddForm);
 
-  [popupEditCloseButton, popupAddCloseButton, popupFullCloseButton].forEach(button => {
-    button.addEventListener('click', (e) => closePopup(e))
-  });
+    [popupEditCloseButton, popupAddCloseButton, popupFullCloseButton].forEach(button => {
+        button.addEventListener('click', (e) => closePopup(e))
+    });
 
-  // eslint-disable-next-line no-undef
-  cards.forEach((elem) => {
-    addCardToDOM(elem);
-  });
+    // eslint-disable-next-line no-undef
+    cards.forEach((elem) => {
+        addCardToDOM(elem);
+    });
 }
 
 const createCard = (elem) => {
-  const name = elem.name;
-  const link = elem.link;
-  const newCard = mestoTemplateContent.cloneNode(true);
-  const newCardImage = newCard.querySelector('.grid-cards__img');
+    const name = elem.name;
+    const link = elem.link;
+    const newCard = mestoTemplateContent.cloneNode(true);
+    const newCardImage = newCard.querySelector('.grid-cards__img');
 
-  newCardImage.src = link;
-  newCardImage.alt = name;
-  newCard.querySelector('.grid-cards__title').textContent = name;
+    newCardImage.src = link;
+    newCardImage.alt = name;
+    newCard.querySelector('.grid-cards__title').textContent = name;
 
-  newCard.querySelector('.grid-cards__img').addEventListener('click', (e) => {
-    popupFullImg.src = e.target.currentSrc;
-    popupFullImg.alt = e.target.alt;
-    popupFull.querySelector('.popup__full-title').textContent = e.target.alt;
-    createPopup('image');
-  })
-  newCard.querySelector('.grid-cards__like').addEventListener('click', (e) => {
-    e.target.classList.toggle('grid-cards__like_liked');
-  });
-  newCard.querySelector('.grid-cards__delete').addEventListener('click', (e) => {
-    // eslint-disable-next-line no-undef
-    cards.splice(cards.findIndex(mesto => mesto.name === e.target.offsetParent.querySelector('.grid-cards__title').innerText), 1)
-    e.target.offsetParent.remove();
-  })
+    newCard.querySelector('.grid-cards__img').addEventListener('click', (e) => {
+        popupFullImg.src = e.target.currentSrc;
+        popupFullImg.alt = e.target.alt;
+        popupFull.querySelector('.popup__full-title').textContent = e.target.alt;
+        createPopup('image');
+    })
+    newCard.querySelector('.grid-cards__like').addEventListener('click', (e) => {
+        e.target.classList.toggle('grid-cards__like_liked');
+    });
+    newCard.querySelector('.grid-cards__delete').addEventListener('click', (e) => {
+        // eslint-disable-next-line no-undef
+        cards.splice(cards.findIndex(mesto => mesto.name === e.target.offsetParent.querySelector('.grid-cards__title').innerText), 1)
+        e.target.offsetParent.remove();
+    })
 
-  return newCard;
+    return newCard;
 }
 
 const addCardToDOM = (elem) => {
-  grid.prepend(createCard(elem));
+    grid.prepend(createCard(elem));
 }
 
 
 const createPopup = (type) => {
-  switch (type) {
-    case 'edit':
-      popupEdit.classList.add('popup_opened');
-      nameInput.value = showName.textContent;
-      subtitleInput.value = showSubtitle.textContent;
-      addCloseListeners(popupEdit);
-      break;
+    switch (type) {
+        case 'edit':
+            popupEdit.classList.add('popup_opened');
+            nameInput.value = showName.textContent;
+            subtitleInput.value = showSubtitle.textContent;
+            addCloseListeners(popupEdit);
+            break;
 
-    case 'add':
-      popupAdd.classList.add('popup_opened');
-      addCloseListeners(popupAdd);
-      break;
+        case 'add':
+            popupAdd.classList.add('popup_opened');
+            addCloseListeners(popupAdd);
+            break;
 
-    case 'image':
-      popupFull.classList.add('popup_opened');
-      addCloseListeners(popupFull);
-      break;
-  }
- 
+        case 'image':
+            popupFull.classList.add('popup_opened');
+            addCloseListeners(popupFull);
+            break;
+    }
+
 }
 
 const addCloseListeners = (popup) => {
-  console.log(popup);
-  document.addEventListener('keydown', (e) => {
-    if(e.key === 'Escape') {
-      closePopup();
-    }
-  })
+    document.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape') {
+            closePopup(e);
+        }
+    })
+    popup.addEventListener('click', (e) => {
+        if (e.target.classList.contains('popup_opened')) {
+            closePopup();
+        }
+    })
 }
 
-const closePopup = (elem) => {
-  console.log(elem);
-  elem.target.closest('.popup').classList.toggle('popup_opened');
-  elem.removeEventListener('keydown');
+
+const closePopup = () => {
+
+    const openedPopup = document.querySelector('.popup_opened');
+    //evt.target.closest('.popup').classList.toggle('popup_opened');
+    if (openedPopup) {
+        openedPopup.classList.remove('popup_opened');
+    }
+
+    document.removeEventListener('keydown', (e) => {
+        if (e.key === 'Escape') {
+            closePopup();
+        }
+    })
 
 }
 
 const saveProfileData = (name, subtitle) => {
-  showName.textContent = name;
-  showSubtitle.textContent = subtitle;
+    showName.textContent = name;
+    showSubtitle.textContent = subtitle;
 }
 
 const submitEditForm = (e) => {
-  e.preventDefault();
-  if (nameInput && subtitleInput) {
-    saveProfileData(nameInput.value, subtitleInput.value);
-  }
-  closePopup(e);
+    e.preventDefault();
+    if (nameInput && subtitleInput) {
+        saveProfileData(nameInput.value, subtitleInput.value);
+    }
+    closePopup(e);
 }
 
 const submitAddForm = (e) => {
-  e.preventDefault();
-  const mestoToPush = { 'name': placeTitleInput.value, 'link': placeLinkInput.value };
-  addCardToDOM(mestoToPush);
-  closePopup(e);
+    e.preventDefault();
+    const mestoToPush = { 'name': placeTitleInput.value, 'link': placeLinkInput.value };
+    addCardToDOM(mestoToPush);
+    closePopup(e);
 }
 
 init();
