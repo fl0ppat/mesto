948c11509f9712c1926a0b28c7bd8c6a76703090
Daniel Khrenov <klaven011@gmail.com>
2021-02-17 10:12:23 +0300

    build: webp support

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: { main: "./src/scripts/index.js" },
+  entry: { main: "./src/pages/index.js" },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
@@ -39,7 +39,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(svg|png|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        test: /\.(svg|png|jpg|gif|webp|woff(2)?|eot|ttf|otf)$/,
         type: "asset/resource",
       },
     ],
