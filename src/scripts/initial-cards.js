bd96822ab58e8872c6129d15ff20cdeee9872a8d
Floppy Di <klaven011@gmail.com>
2021-02-18 12:33:28 +0300

    feat: cards placeholder

--- src/scripts/initial-cards.js
+++ src/scripts/initial-cards.js
@@ -1,29 +1,12 @@
-// eslint-disable-next-line no-unused-vars
-const cards = [
+export const initialCards = [
   {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    name: "Cat",
+    link: "../vendor/placeholder.gif",
   },
   {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-  {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+    name: "Another cat",
+    link: "../vendor/placeholder.gif",
   },
 ];
 
-export { cards };
+export default initialCards;
